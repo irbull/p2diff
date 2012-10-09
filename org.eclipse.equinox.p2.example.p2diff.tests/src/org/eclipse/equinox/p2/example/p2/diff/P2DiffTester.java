faf8dd4dc4f411e4922036fde1cebb323d26cc20
Ian Bull <irbull@eclipsesource.com>
2012-10-09 12:47:43 -0700

    Added stubs for testing.

---
+++ org.eclipse.equinox.p2.example.p2diff.tests/src/org/eclipse/equinox/p2/example/p2/diff/P2DiffTester.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ *  Copyright (c) 2012 EclipseSource
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ * 
+ *  Contributors:
+ *     EclipseSource - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.equinox.p2.example.p2.diff;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class P2DiffTester {
+
+	@Test
+	public void testGetRelativeComplementA() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRelativeComplementB() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRepositoryA() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRepositoryB() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRepositoryALocation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRepositoryBLocation() {
+		fail("Not yet implemented");
+	}
+
+}
